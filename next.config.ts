a0552076d30c6fdb1b1ae5f05f3803084f43600f
ParkMinHo <aoiyuki4u@ipartners.co.kr>
2026-05-19 15:46:49 +0900

    next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -7,10 +7,6 @@ const nextConfig: NextConfig = {
     // ⚠️ 빌드 시 타입 에러가 있어도 버셀 배포를 강제로 진행하도록 허용합니다.
     ignoreBuildErrors: true,
   },
-  eslint: {
-    // 빌드 시 ESLint 경고/에러로 배포가 막히는 현상 방지
-    ignoreDuringBuilds: true,
-  },
 };
 module.exports = {
   images: {
